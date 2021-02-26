f1b247dfeae1ec3b7fff74ed04499f6988a5a8ff
{shiva} <girishiva93@gmail.com>
2021-02-26 22:26:22 +0545

    Update

--- JS/everestBaseCampTrek.js
+++ JS/everestBaseCampTrek.js
@@ -5,10 +5,10 @@ window.addEventListener("scroll",bringmenu);
 function bringmenu(){
     if(document.body.scrollTop>400 || document.documentElement.scrollTop>500){
         document.getElementById("discount-offer-table").style.display = 'none';
-        document.getElementById("explain-trip").style.display='none';
+        // document.getElementById("explain-trip").style.display='none';
     }
     else{
         document.getElementById("discount-offer-table").style.display = 'block';
-        document.getElementById("explain-trip").style.display='block';
+        // document.getElementById("explain-trip").style.display='block';
     }
 }
