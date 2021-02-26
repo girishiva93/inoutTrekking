b6d04a8cfcffe1e34d4e067e02dbb37e17448704
{shiva} <girishiva93@gmail.com>
2021-02-26 22:03:03 +0545

    Update

--- JS/everestBaseCampTrek.js
+++ JS/everestBaseCampTrek.js
@@ -1,35 +1,14 @@
 console.log("Hello");
 
-// $(window).scroll(function() {
-//     if ($(this).scrollTop() > 100) {
-//         // $('.discount-offer-price').hide();
-//         $('discount-offer-price').removeClass("discount-offer-price");
-//         document.getElementsByClassName("discount-offer-price").style.display = "none";
-//     } else {
-//         $('discount-offer-price').hideClass("discount-offer-price");
-//         document.getElementsByClassName("discount-offer-price").style.display = "block";
-//     }
-//  });
-// $(window).trigger('scroll');
-
-// var hideData = document.getElementsByClassName("discount-offer-price");
-// window.addEventListener("scroll",function(){
-//     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-//     if(scrollTop>100){
-//         hideData.style.display = 'block';
-//     }
-//     else{
-//         hideData.style.display = 'none';
-//     }
-// })
-
 window.addEventListener("scroll",bringmenu);
 
 function bringmenu(){
     if(document.body.scrollTop>400 || document.documentElement.scrollTop>500){
         document.getElementById("discount-offer-table").style.display = 'none';
+        document.getElementById("explain-trip").style.display='none';
     }
     else{
         document.getElementById("discount-offer-table").style.display = 'block';
+        document.getElementById("explain-trip").style.display='block';
     }
 }
